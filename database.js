4126210e24736291f1aa5a28c05631bceff2e9bd
Taciane Santos <taciane.ssanttos@gmail.com>
2021-07-11 12:02:26 -0300

    Estruturando o projeto

---
+++ database.js
@@ -0,0 +1,51 @@
+const livro = [
+    {
+        id:1,
+        nome: "O próximo passo",
+        autor: "Marcelo César",
+        categoria: "Romance Espírita",
+        paginas: 310,
+        recomenda: true,
+        leu: true,
+    },
+    {
+        id:2,
+        nome:"O Vencedor Não Está Só",
+        autor:"Paulo Coelho",
+        categoria: "Romance",
+        paginas: 300,
+        recomenda:true,
+        leu:true,
+
+    },
+    {
+        id:3,
+        nome: "Como Eu Era Antes de Você",
+        autor: "Jojo Moyers",
+        categoria: "Romance",
+        paginas: 300,
+        recomenda:true,
+        leu:true,
+    },
+    {
+        id:4,
+        nome:"Depois de Você",
+        autor: "Jojo Moyers",
+        categoria: "Romance",
+        paginas: 300,
+        recomenda:false,
+        leu:false,
+    },
+    {
+        id:5,
+        nome:"Diário deepois de Você",
+        autor: "Jojo Moyers",
+        categoria: "Romance",
+        paginas: 300,
+        recomenda:false,
+        leu:false,
+    }
+
+];
+
+module.exports = livro;
